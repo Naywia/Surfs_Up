985d428e2ab127f2a56e25ba8783fe712b88e12e
Naywia <48431873+Naywia@users.noreply.github.com>
2024-09-04 13:21:44 +0200

    Carousel go round n' round 🎠🎠🎠

--- SurfsUp/wwwroot/js/site.js
+++ SurfsUp/wwwroot/js/site.js
@@ -4,10 +4,17 @@
 // Write your JavaScript code.
 
 function goTo(id) {
-    let destination = document.querySelector(id);
-    let yCoord = destination.offsetTop - 80;
-    window.scrollTo({
-      top: yCoord,
-      behavior: "smooth"
-    });
-  }
+  let destination = document.querySelector(id);
+  let yCoord = destination.offsetTop - 80;
+  window.scrollTo({
+    top: yCoord,
+    behavior: "smooth"
+  });
+}
+
+const carouselElement = document.querySelector('#surf-carousel')
+
+const carousel = new bootstrap.Carousel(carouselElement, {
+  interval: 7500,
+  touch: false
+})
