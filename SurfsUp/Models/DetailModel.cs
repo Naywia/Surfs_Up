1506ce356c8332acd109ca904d0dc31a06db4807
Naywia <48431873+Naywia@users.noreply.github.com>
2024-09-05 11:33:28 +0200

    Annnnnd a little extra

--- SurfsUp/Models/DetailModel.cs
+++ SurfsUp/Models/DetailModel.cs
@@ -4,4 +4,5 @@ public class DetailModel
 {
     public List<EquipmentModel> Equipment { get; set; }
     public List<SuitModel> Suits { get; set; }
+    public List<AddonModel> Addons {get; set;}
 }
