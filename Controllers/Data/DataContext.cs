f7b75bff4cad0f9e9a594c33b9d681ac126932a2
itsAfeat <itsafeat@gmail.com>
2024-09-17 12:20:28 +0200

    Removed the nullability from the DbSet's as they added a bajillion warnings, also found how one adds co authors since last commit, so now all of yall should be added to the db commit and such... tihi
    Co-Authored-By: Naywia <48431873+Naywia@users.noreply.github.com>
    Co-Authored-By: mjcrl <145342512+mjcrl@users.noreply.github.com>
    Co-Authored-By: JooMooCN <60666905+JooMooCN@users.noreply.github.com>

--- Controllers/Data/DataContext.cs
+++ Controllers/Data/DataContext.cs
@@ -5,10 +5,10 @@ namespace SurfsUp.Data
 {
     class DataContext : DbContext
     {
-        public static DbSet<AddonModel>? Addons { get; set; }
-        public static DbSet<BookingModel>? Bookings { get; set; }
-        public static DbSet<EquipmentModel>? Equipments { get; set; }
-        public static DbSet<SuitModel>? Suits { get; set; }
+        public static DbSet<AddonModel> Addons { get; set; }
+        public static DbSet<BookingModel> Bookings { get; set; }
+        public static DbSet<EquipmentModel> Equipments { get; set; }
+        public static DbSet<SuitModel> Suits { get; set; }
 
         public DataContext()
         {}
