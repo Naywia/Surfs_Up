c0df26920561a9bdfa0b60ef7c13576dbcd683e8
itsAfeat <itsafeat@gmail.com>
2024-10-11 13:46:47 +0200

    Restored the DB data... whoopsie

---
+++ Surfs_Up_WebAPI/Migrations/20241011114527_DBRestoreAttempt.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Surfs_Up_WebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class DBRestoreAttempt : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
